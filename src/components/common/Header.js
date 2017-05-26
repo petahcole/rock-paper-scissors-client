19633526f59cbc32ffe04a15ae410c040b9a867d
petahcole <cbatson89@gmail.com>
2017-05-26 10:32:31 -0600

    set up components

---
+++ src/components/common/Header.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react';
+
+const Header = () =>  {
+  return  (
+    <nav className="navbar navbar-fixed-top">
+      <div className="container">
+        <h1>Rock | Paper | Scissors</h1>
+      </div>
+    </nav>
+  )
+}
